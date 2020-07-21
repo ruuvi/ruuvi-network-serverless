2dfd550bf120f56e146b6d9165c67b530a6291c8
Sami Anttila <sami@meetsoci.com>
2020-07-22 01:13:53 +0300

    Maximum batch sizes via asynchronous batch inserts

--- writeToDatabase/index.js
+++ writeToDatabase/index.js
@@ -5,6 +5,18 @@ const dynamoHelper = require('dynamoHelper.js');
 exports.handler = async (event) => {
     // Flatten into an array
     let flattenedData = [];
+
+    function sendBatch(data) {
+         const batch = dynamoHelper.getDynamoBatch(flattenedData);
+            
+        return dynamo.batchWriteItem(batch, function(err, data) {
+            if (err) {
+                console.error("Error", err);
+            }
+        }).promise();
+    }
+    
+    let uploadBatchPromises = [];
     
     for (const { messageId, body, messageAttributes } of event.Records) {
         const gwmac = messageAttributes.gwmac.stringValue;
@@ -20,19 +32,21 @@ exports.handler = async (event) => {
             tags[key].received = timestamp;
             
             flattenedData.push(tags[key]);
+            
+            if (flattenedData.length >= 25) {
+               uploadBatchPromises.push(sendBatch(flattenedData));
+               flattenedData = [];
+            }
         });
     }
-
-    const batch = dynamoHelper.getDynamoBatch(flattenedData);
-
+    if (flattenedData.length > 0) {
+        uploadBatchPromises.push(sendBatch(flattenedData));
+    }
+  
     // Note: async's in Lambdas should always be awaited as exiting the function
     // pauses the execution context and there is no guarantee that the same one
-    // will be resumed in the future.
-    await dynamo.batchWriteItem(batch, function(err, data) {
-        if (err) {
-            console.error("Error", err);
-        }
-    }).promise();
-    
+    // will be resumed in the future.  
+    await Promise.all(uploadBatchPromises);
+
     return `Successfully processed ${event.Records.length} messages.`;
 };
