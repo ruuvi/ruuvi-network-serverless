72b514fdefcc4d1937d03f32b960dc54a5951b9f
Sami Anttila <sami@muhwu.com>
2020-08-11 04:46:08 +0300

    getSensorData authorization

--- UserApi/getSensorData.js
+++ UserApi/getSensorData.js
@@ -1,18 +1,33 @@
 const gatewayHelper = require('Helpers/gatewayHelper');
 const dynamoHelper = require('Helpers/dynamoHelper');
+const validator = require('Helpers/validator');
+const auth = require('Helpers/authHelper');
+
+const mysql = require('serverless-mysql')({
+    config: {
+        host     : process.env.ENDPOINT,
+        database : process.env.DATABASE,
+        user     : process.env.USERNAME,
+        password : process.env.PASSWORD
+    }
+});
 
 exports.handler = async (event, context) => {
     // Authorization
-    if (process.env.ACCESS_KEY !== "" && event.headers.authorization !== 'Bearer ' + process.env.ACCESS_KEY) {
-        // Forbidden
-        return gatewayHelper.forbidden();
+    let user = null;
+    if (process.env.REQUIRE_LOGIN == 1) {
+        const authInfo = event.headers.Authorization;
+        user = await auth.authorizedUser(authInfo);
+        if (!user) {
+            return gatewayHelper.forbiddenResponse();
+        }
     }
     
     // Validation
     if (
-        !event.queryStringParameters.hasOwnProperty('tag')
-        || event.queryStringParameters.tag === null
-        || event.queryStringParameters.tag.length < 8) {
+        !event.queryStringParameters
+        || !event.queryStringParameters.hasOwnProperty('tag')
+        || !validator.validateToken(event.queryStringParameters.tag)) {
 
         // Invalid request
         return gatewayHelper.invalid();
@@ -20,6 +35,25 @@ exports.handler = async (event, context) => {
 
     const tag = event.queryStringParameters.tag;
     
+    if (user) {
+        const hasClaim = await mysql.query(
+            `SELECT claim_id
+            FROM claimed_tags
+            WHERE
+                user_id = ${user.id}
+                AND tag_id = '${tag}'
+            UNION
+            SELECT share_id
+            FROM shared_tags
+            WHERE
+                user_id = ${user.id}
+                AND tag_id = '${tag}'`
+        );
+        if (hasClaim.length === 0) {
+            return gatewayHelper.errorResponse(gatewayHelper.HTTPCodes.FORBIDDEN, "Cannot access tag.");
+        }
+    }
+
     const dataPoints = await dynamoHelper.getSensorData(tag, process.env.DEFAULT_RESULTS);
     if (dataPoints.length === 0) {
         // Not found
