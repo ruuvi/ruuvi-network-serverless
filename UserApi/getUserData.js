bc85814534eac27f7a81a6bdaba926514a8dfe22
Sami Anttila <sami@meetsoci.com>
2020-10-05 02:42:35 +0300

    API response to lower case

--- UserApi/getUserData.js
+++ UserApi/getUserData.js
@@ -18,15 +18,15 @@ exports.handler = async (event, context) => {
 
     const tags = await mysql.query({
         sql: `SELECT
-                tag_id AS Tag,
-                true AS Owner,
+                tag_id AS tag,
+                true AS owner,
                 picture AS picture
             FROM tags
             WHERE owner_id = ?
             UNION
             SELECT
-                tag_id AS Tag,
-                false AS Owner,
+                tag_id AS tag,
+                false AS owner,
                 '' AS picture
             FROM shared_tags
             WHERE user_id = ?`,
