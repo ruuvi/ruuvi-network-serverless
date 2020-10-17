9c01ee0c78dcd6542f9e5c008a27307cb58980e3
Sami Anttila <sami@meetsoci.com>
2020-10-17 05:51:50 +0300

    Indents

--- tests/unit/verifyToken.test.js
+++ tests/unit/verifyToken.test.js
@@ -1,5 +1,5 @@
 const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2MDE2NDUyMDUsImlhdCI6MTYwMTY0NDMwNSwiZGF0YSI6eyJlbWFpbCI6ImF3d3dpbkBnbWFpbC5jb20iLCJ0eXBlIjoicmVzZXQifX0.LIsTEhO5XZOVVUBqXrwsHwg1vySIFP848ovMX_p4e8k';
-const secret = '6AGHfcqXM1IcsyrdaElWqLsWZKmg39ovSaKiVsi2E96cZjqCZ8ioJlATcM3CT6aE';
+const secret = '6AGHfcqXM1IcsyrdaElWqLsWZKmg39ovSaKiVsi2Ex6cZjqCZ8ioJlATcM3CT6aE';
 const jwt = require('jsonwebtoken')
 
 test('verify throws on expired token', () => {
