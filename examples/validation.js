086a3d8ea47329c2db6f8c47f12197fad2aa2084
Otso Jousimaa <otso@ojousima.net>
2021-12-02 18:03:28 +0200

    Convert to spaces, remove unused function

--- examples/validation.js
+++ examples/validation.js
@@ -4,130 +4,130 @@ const deviceAddr = '5678';
 const nonce = 'nonsense!';
 const timestamp = Date.now();
 const postData = {
-  data:	{
+  data: {
     coordinates: '',
     timestamp: 133,
     gwmac: 'abbacd',
-    tags:	{
-      '76EA4D1D1021':	{
-        rssi:	-76,
+    tags: {
+      '76EA4D1D1021': {
+        rssi: -76,
         timestamp: 1595333605932,
-        data:	'02011A020A0C0AFF4C001005031C6E57DD'
+        data: '02011A020A0C0AFF4C001005031C6E57DD'
       },
-      '5575B388898D':	{
-        rssi:	-52,
+      '5575B388898D': {
+        rssi: -52,
         timestamp: 1595198605932,
-        data:	'1EFF06000109200230E6726EBD7FD084DFC33558D954149C321F735CC51159'
+        data: '1EFF06000109200230E6726EBD7FD084DFC33558D954149C321F735CC51159'
       },
-      '6C4008A1C25D':	{
-        rssi:	-48,
+      '6C4008A1C25D': {
+        rssi: -48,
         timestamp: 1595198605933,
-        data:	'0201060AFF4C0010054B1C6C9ED5'
+        data: '0201060AFF4C0010054B1C6C9ED5'
       },
-      '655D7925361D':	{
-        rssi:	-52,
+      '655D7925361D': {
+        rssi: -52,
         timestamp: 1595198605934,
-        data:	'02010613FF4C000C0E00BC32D7E1B0C296ADA26E8632C9'
+        data: '02010613FF4C000C0E00BC32D7E1B0C296ADA26E8632C9'
       },
-      '16FD6FF63364':	{
-        rssi:	-62,
+      '16FD6FF63364': {
+        rssi: -62,
         timestamp: 1595198605936,
-        data:	'1EFF0600010920029E258660E0B062F2FFB868D9337736E2CD2038057DB9BD'
+        data: '1EFF0600010920029E258660E0B062F2FFB868D9337736E2CD2038057DB9BD'
       },
-      F63A84A8E4CF:	{
-        rssi:	-24,
+      F63A84A8E4CF: {
+        rssi: -24,
         timestamp: 1595198605933,
-        data:	'0201041BFF990405154636CDC88B004C002403F079B6FFFFFFF63A84A8E4CF'
+        data: '0201041BFF990405154636CDC88B004C002403F079B6FFFFFFF63A84A8E4CF'
       },
-      '654FF6FE52A8':	{
-        rssi:	-27,
+      '654FF6FE52A8': {
+        rssi: -27,
         timestamp: 1595198605933,
-        data:	'0201060AFF4C001005451CED6715'
+        data: '0201060AFF4C001005451CED6715'
       },
-      '47B73797431A':	{
-        rssi:	-39,
+      '47B73797431A': {
+        rssi: -39,
         timestamp: 1595198605933,
-        data:	'02011A020A0C0BFF4C0010060B1E4B03D47C'
+        data: '02011A020A0C0BFF4C0010060B1E4B03D47C'
       },
-      '442CFAA838B8':	{
-        rssi:	-50,
+      '442CFAA838B8': {
+        rssi: -50,
         timestamp: 1595198605933,
-        data:	'1EFF0600010F2002CFBF43DCD33C599D71FFE3E2267A581766B8F09F2008DC'
+        data: '1EFF0600010F2002CFBF43DCD33C599D71FFE3E2267A581766B8F09F2008DC'
       },
-      F40F2438E2E9:	{
-        rssi:	-62,
+      F40F2438E2E9: {
+        rssi: -62,
         timestamp: 1595198605933,
-        data:	'0201060AFF4C0010054B1C263A26'
+        data: '0201060AFF4C0010054B1C263A26'
       },
-      '7C0E31B04AD5':	{
-        rssi:	-62,
+      '7C0E31B04AD5': {
+        rssi: -62,
         timestamp: 1595198605933,
-        data:	'02010613FF4C000C0E08EACB2EA8DA2B709A34DF1FD2B2'
+        data: '02010613FF4C000C0E08EACB2EA8DA2B709A34DF1FD2B2'
       },
-      '4A251258A167':	{
-        rssi:	-45,
+      '4A251258A167': {
+        rssi: -45,
         timestamp: 1595198605933,
-        data:	'02011A020A0C0BFF4C001006031E82AC36B5'
+        data: '02011A020A0C0BFF4C001006031E82AC36B5'
       },
-      '7C5B1B365242':	{
-        rssi:	-72,
+      '7C5B1B365242': {
+        rssi: -72,
         timestamp: 1595198605933,
-        data:	'0201060AFF4C0010050B1C60D1AF'
+        data: '0201060AFF4C0010050B1C60D1AF'
       },
-      '791BD51E1A27':	{
-        rssi:	-51,
+      '791BD51E1A27': {
+        rssi: -51,
         timestamp: 1595198605933,
-        data:	'02011A020A0C0BFF4C001006471E1B522613'
+        data: '02011A020A0C0BFF4C001006471E1B522613'
       },
-      '323CB5C0FB57':	{
-        rssi:	-73,
+      '323CB5C0FB57': {
+        rssi: -73,
         timestamp: 1595198605933,
-        data:	'1EFF060001092002BB132272588BDD97624C64193F2DF14528D59D26D56D07'
+        data: '1EFF060001092002BB132272588BDD97624C64193F2DF14528D59D26D56D07'
       },
-      '194488332F62':	{
-        rssi:	-87,
+      '194488332F62': {
+        rssi: -87,
         timestamp: 1595198605933,
-        data:	'1EFF06000109200277931B5F7B618C7D5396EAACDB4E476FC2361A6FD44932'
+        data: '1EFF06000109200277931B5F7B618C7D5396EAACDB4E476FC2361A6FD44932'
       },
-      DAB59BDFA9BB:	{
-        rssi:	-56,
+      DAB59BDFA9BB: {
+        rssi: -56,
         timestamp: 1595198605933,
-        data:	'0201041BFF99040512954B2FC8880074FF7004006976E2A7A6DAB59BDFA9BB'
+        data: '0201041BFF99040512954B2FC8880074FF7004006976E2A7A6DAB59BDFA9BB'
       },
-      '15082A6773B2':	{
-        rssi:	-73,
+      '15082A6773B2': {
+        rssi: -73,
         timestamp: 1595198605933,
-        data:	'1EFF0600010920025FDE9F9234847B66681BB2CC17209976F602936034AE46'
+        data: '1EFF0600010920025FDE9F9234847B66681BB2CC17209976F602936034AE46'
       },
-      '74F6D8A52FB4':	{
-        rssi:	-68,
+      '74F6D8A52FB4': {
+        rssi: -68,
         timestamp: 1595198605933,
-        data:	'02011A020A0C0BFF4C001006131E56BCEC09'
+        data: '02011A020A0C0BFF4C001006131E56BCEC09'
       },
-      '0016A40FB3CD':	{
-        rssi:	-62,
+      '0016A40FB3CD': {
+        rssi: -62,
         timestamp: 1595198605933,
-        data:	'0201060319E00016FF150264E0001E06F054ABE5BC4CB681BC11AFEB0BC2'
+        data: '0201060319E00016FF150264E0001E06F054ABE5BC4CB681BC11AFEB0BC2'
       },
-      '31B04AD52CE9':	{
-        rssi:	-60,
+      '31B04AD52CE9': {
+        rssi: -60,
         timestamp: 1595198605933,
-        data:	'0201060AFF4C0010054B1C263A26'
+        data: '0201060AFF4C0010054B1C263A26'
       },
-      D70411EBCE76:	{
-        rssi:	-73,
+      D70411EBCE76: {
+        rssi: -73,
         timestamp: 1595198605933,
-        data:	'031900000201061107275125077B182691364CDD843CFC39D8'
+        data: '031900000201061107275125077B182691364CDD843CFC39D8'
       },
-      '657C2C25361D':	{
-        rssi:	-77,
+      '657C2C25361D': {
+        rssi: -77,
         timestamp: 1595198605933,
-        data:	'02010613FF4C000C0E00BC32D7E1B0C296ADA26E8632C9'
+        data: '02010613FF4C000C0E00BC32D7E1B0C296ADA26E8632C9'
       },
-      '3AC40F493821':	{
-        rssi:	-75,
+      '3AC40F493821': {
+        rssi: -75,
         timestamp: 1595198605933,
-        data:	'1EFF060001092002E76BF96742B4855B5E1FE0DAB4367FC6C0D1B8D37E8812'
+        data: '1EFF060001092002E76BF96742B4855B5E1FE0DAB4367FC6C0D1B8D37E8812'
       }
     }
   }
@@ -147,10 +147,6 @@ const createSignature = (data, nonce, timestamp, secret) => {
     .update(signatureBody)
     .digest('hex');
 };
-
-function sleep (ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
 
 async function testSignature (postData, secret) {
   const https = require('https');
