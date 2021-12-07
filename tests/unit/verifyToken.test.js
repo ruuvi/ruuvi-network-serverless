8397e645468e998246aae2c4f6ff5fb7ef450401
Otso Jousimaa <otso@ojousima.net>
2021-12-07 15:04:29 +0200

    Lint verifyToken.test.js

--- tests/unit/verifyToken.test.js
+++ tests/unit/verifyToken.test.js
@@ -1,3 +1,4 @@
+const { test, expect } = require('@jest/globals');
 const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2MDE2NDUyMDUsImlhdCI6MTYwMTY0NDMwNSwiZGF0YSI6eyJlbWFpbCI6ImF3d3dpbkBnbWFpbC5jb20iLCJ0eXBlIjoicmVzZXQifX0.LIsTEhO5XZOVVUBqXrwsHwg1vySIFP848ovMX_p4e8k';
 const secret = '6AGHfcqXM1IcsyrdaElWqLsWZKmg39ovSaKiVsi2Ex6cZjqCZ8ioJlATcM3CT6aE';
 const jwt = require('jsonwebtoken');
