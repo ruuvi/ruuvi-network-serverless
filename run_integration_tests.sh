9bbeb0929c8186aa63ee2dd906ce1a9e1686adea
Otso Jousimaa <otso@ojousima.net>
2022-02-14 09:10:23 +0200

    Unify parameter passing to scripts #192 (#195)

--- run_integration_tests.sh
+++ run_integration_tests.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
-if [ $# -eq 0 ]
+
+#https://stackoverflow.com/questions/192249/how-do-i-parse-command-line-arguments-in-bash
+for i in "$@"; do
+  case $i in
+    -s=*|--stage=*)
+      RSTAGE="${i#*=}"
+      shift # past argument=value
+      ;;
+    -*|--*)
+      echo "Unknown option $i"
+      exit 1
+      ;;
+    *)
+      ;;
+  esac
+done
+
+if [ -z ${RSTAGE+x} ];
 then
-  RSTAGE=dev
-else
-  RSTAGE=$1
+    RSTAGE=dev
 fi
+
 STAGE=$RSTAGE IS_INTEGRATION_TEST=true jest --bail --runInBand --coverage
 
 date
