4d1d81bbbe036f3863b91106ed9b77f2f136ef15
Otso Jousimaa <otso@ojousima.net>
2022-04-20 16:23:31 +0300

    Readback redis value for checking

--- Helpers/wrapper.js
+++ Helpers/wrapper.js
@@ -81,6 +81,8 @@ const gatewayWrapper = async (func, event, context, requireAuth = true) => {
           console.error(`REDIS failed to store timestamp, result was ${redisResult}`);
         } else {
           console.info(`${gwMac} - Invalid signature: ${signature}, Logging timestamp ${timestring} with key ${keyString}`);
+          const readbackResult = await redis.get(keyString);
+          console.info(`${gwMac} - Readback timestamp ${readbackResult}`);
         }
         // Check signature if signature is present, or if it is enforced to be present
         if (parseInt(process.env.ENFORCE_SIGNATURE) === 1 || signature !== null) {
